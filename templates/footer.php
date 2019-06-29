4b311c5b7def23c426f1b4d8e273d699539733fc
duke speaks <dukespeaks@debian9Acer.debian9acer.org>
2019-06-29 16:21:30 -0500

    13042019 password_hash

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@
 	<div class="container p-1">
            <div class="row text-center text-white">
              <div class="col ml-auto">
-                <p>Stamp is a timelines maker. Uses PDO connection to MySQL tables (made them with phpMyAdmin), HTML5, CSS3 and BootStrap 4. Stamp is a development in progress on Debian 9 and LAMPP stack.<br><time datetime="2019-04-7" pubdate>Last Update: April 7th, 2019</time></p>
+                <p>Stamp is a timelines maker. Uses PDO connection to MySQL tables (made them with phpMyAdmin), HTML5, CSS3 and BootStrap 4. Stamp is a development in progress on Debian 9 and LAMPP stack.<br><time datetime="2019-04-7" pubdate>Last Update: April 13th, 2019</time></p>
              </div>
            </div>
 	   <div class="d-flex flex-grow justify-content-center">
